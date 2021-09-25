28f458f9c8ebc1bcd8fcf63102db565179a0a541
balutharan <balutharan91@gmail.com>
2021-09-25 15:02:53 +0530

    added few lines of code

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,20 @@
+package org.sample;
+
+public class Sample {
+ public static void main(String[] args) {
+	 
+}
+private void Tc1() {
+	System.out.println("5");
+    
+	Sample b = new Sample();
+	b.Tc1();
+	
+	
+	
+	
+}
+ 
+
+ 
+}
